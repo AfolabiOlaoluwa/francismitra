08063333b18cd3572e24fdae9a5cd4e4039c3680
Francis Mitra <francis.mitra@quallsbenson.com>
2014-08-03 18:52:56 -0400

    Set dynamic urls for menu

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from portfolio.models import Images, Categories, Videos
 
+# Mixin to create menu
 class LayoutView(object):
 	template_name = 'portfolio/layout.html'
 

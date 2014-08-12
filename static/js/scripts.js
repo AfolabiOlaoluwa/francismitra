1fc42d23dde5a247d728992ed5320345666aaac1
Francis Mitra <francis.mitra@quallsbenson.com>
2014-08-12 00:00:33 -0400

    Clean up mobile and desktop styles

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
 	$.fn.animateMenu = function() {
 		var height = this.css('height');
 		if(height == '50px') {
-			this.animate({'height':'407px'}, 600);
+			this.animate({'height':'362px'}, 600);
 		} else {
 			this.animate({'height':'50px'}, 600);
 		}

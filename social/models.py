80f95b496bc20050c5861cd326c95ee98f57425f
Francis Mitra <francis.mitra@quallsbenson.com>
2014-09-13 16:12:43 -0400

    Refactor all social views and models

--- social/models.py
+++ social/models.py
@@ -1,6 +1,7 @@
 import urllib2
 import json
 from django.db import models
+from instagram import client
 
 class PersonalInstagram(object):
 	def __init__(self, user_id, client_id):
@@ -14,6 +15,38 @@ class PersonalInstagram(object):
 
 		return data
 
+class AuthenticateInstagram(object):
+	def __init__(self, client_id, client_secret, redirect_uri):
+		self.client_id     = client_id
+		self.client_secret = client_secret
+		self.redirect_uri  = redirect_uri
 
-	
+		CONFIG = {
+			'client_id': self.client_id,
+			'client_secret': self.client_secret,
+			'redirect_uri': self.redirect_uri
+		}
+
+		self.unauthenticated_api = client.InstagramAPI(**CONFIG)
+
+	def get_authorization_url(self):
+		try:
+			url = self.unauthenticated_api.get_authorize_url(scope=["likes","comments"])
+			return url
+		except Exception, e:
+			url = '#'
+			return url
+
+	def find_access_token(self, code):
+		try:
+			access_token, user_info = self.unauthenticated_api.exchange_code_for_access_token(code)
+			return access_token
+		except Exception, e:
+			print e
+
+
+
+
+
+
 
--- social/views.py
+++ social/views.py
@@ -3,50 +3,46 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.detail import BaseDetailView
 from django import http
-from instagram import client
-from social.models import PersonalInstagram
+from social.models import PersonalInstagram, AuthenticateInstagram
 from portfolio.views import LayoutView
 
 INSTAGRAM_CONFIG = {
+	'user_id': '2968231',
 	'client_id': 'af80dd4c67de439fba77ac4c4743ead0',
 	'client_secret': '1f661d6538c44c9a83ff279bc350bb75',
-	'redirect_uri': 'http://127.0.0.1:8000/social'
+	'redirect_uri': 'http://127.0.0.1:8000/social',
 }
 
-unauthenticated_api = client.InstagramAPI(**INSTAGRAM_CONFIG)
-
 class JSONResponseMixin(object):
-    # Returns a JSON response containing 'context' as payload
-    def render_to_response(self, context):
-        return self.get_json_response(self.convert_context_to_json(context))
+	# Returns a JSON response containing 'context' as payload
+	def render_to_response(self, context):
+		return self.get_json_response(self.convert_context_to_json(context))
 
-    # Construct an `HttpResponse` object.
-    def get_json_response(self, content, **httpresponse_kwargs):
-        return http.HttpResponse(content,
+	# Construct an `HttpResponse` object.
+	def get_json_response(self, content, **httpresponse_kwargs):
+		return http.HttpResponse(content,
                                  content_type='application/json',
                                  **httpresponse_kwargs)
 
-    # Convert the context dictionary into a JSON object
-    def convert_context_to_json(self, context):
-        return json.dumps(context)
+	# Convert the context dictionary into a JSON object
+	def convert_context_to_json(self, context):
+		return json.dumps(context)
 
 '''
-Personal Instagram feed for view consumption.
-Not tied to the authenticated API 
+Personal Instagram feed rendered as JSON response
 '''		
 class InstagramFeed(JSONResponseMixin, BaseDetailView):
 	def get(self, request, *args, **kwargs):
-	    my_user_id        = '2968231'
-	    my_client_id      = 'af80dd4c67de439fba77ac4c4743ead0'
-	    my_instagram      = PersonalInstagram(my_user_id, my_client_id)
+		my_instagram = PersonalInstagram(INSTAGRAM_CONFIG['user_id'], 
+			                             INSTAGRAM_CONFIG['client_id'])
 	    
-	    try: 
-	    	json_response = my_instagram.find_feed()
-	    except Exception, e:
-	    	json_response = {'failed':'Unable to find feed'}
-	    
-	    context = json_response
-	    return self.render_to_response(context)
+		try: 
+			json_response = my_instagram.find_feed()
+		except Exception, e:
+			json_response = {'failed':'Unable to find feed'}
+
+		context = json_response
+		return self.render_to_response(context)
 
 '''
 Allow visitors the option to connect via
@@ -59,32 +55,24 @@ class SocialView(LayoutView, TemplateView):
 		context = super(SocialView, self).get_context_data(**kwargs)
 		context['page_title'] = 'Social'
 
-		# Create button to authenticate users
-		try:
-			url = unauthenticated_api.get_authorize_url(scope=["likes","comments"])
-			context['link'] = url
-		except Exception, e:
-			context['link'] = '#'
-			print e  
+		# Begin authentication to allow viewers to 'comment' and 'like' media
+		authenticate_user = AuthenticateInstagram(INSTAGRAM_CONFIG['client_id'], 
+			                                      INSTAGRAM_CONFIG['client_secret'], 
+			                                      INSTAGRAM_CONFIG['redirect_uri'])
 
-		# Code parameter is available when attempting to authenticate user
-		try:
-			code = self.request.GET.get('code')
-			access_token, user_info = unauthenticated_api.exchange_code_for_access_token(code)
-			if not access_token:
-				return 'No access token available'
-			self.request.session['access_token'] = access_token
-		except Exception, e:
-			print e
+		# Check if code parameter is available and retrieve token
+		if self.request.GET.get('code'):
+			code = self.request.GET.get('code')	
 
-		# If session has been created, enable authenticated features
-		if self.request.session.get('access_token'):
-			check_session = self.request.session.get('access_token')
-			api = client.InstagramAPI(access_token=check_session)
+			try:
+				access_token = authenticate_user.find_access_token(code)
+				self.request.session['access_token'] = access_token
+			except Exception, e:
+				print 'Code not available'
 
-			# Results for testing...
-			# context['test']          = check_session
-			# context['popular_media'] = api.user_recent_media(count=20)
+		# Create visible button to authenticate users
+		if not self.request.session.get('access_token'):
+			context['link'] = authenticate_user.get_authorization_url()
 
 		return context
 

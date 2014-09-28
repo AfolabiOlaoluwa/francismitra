d9c0483988a305c15de45c8f55692df3ffad7d5a
Francis Mitra <francis.mitra@quallsbenson.com>
2014-09-27 20:39:14 -0400

    Fix routes in portfolio

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,16 +6,16 @@ urlpatterns = patterns('',
 		views.IndexView.as_view(), 
 		name='index'
 	),
-	url(r'^(?P<slug>\w+)/$', 
-		views.CategoryView.as_view(), 
-		name='categories'
-	),
-	url(r'^resume', 
+	url(r'^resume/', 
 		views.ResumeView.as_view(), 
 		name='resume'
 	),
-	url(r'^info', 
+	url(r'^info/', 
 		views.InfoView.as_view(), 
 		name='info'
+	),
+	url(r'^(?P<slug>\w+)/$', 
+		views.CategoryView.as_view(), 
+		name='categories'
 	),
 )

bb7e971df40cdf7ebcc2ef35b27393f6d0eedbdf
Francis Mitra <francismitra@gmail.com>
2014-10-04 15:38:29 -0400

    separate logic between backbone social view and social model

--- static/scripts/social.js
+++ static/scripts/social.js
@@ -5,7 +5,47 @@ var social = {}
  * MODELS
  *
  */
-social.InstagramModel = Backbone.Model;
+social.InstagramModel = Backbone.Model.extend({
+	likeMedia: function(target) {
+		var target       = target,
+			media_id     = this.attributes.id,
+			media_count = this.attributes.likes.count,
+			media_like   = '/social/media_like?id=';
+
+		var likeMediaSuccess = function(target) {
+			var target = target,
+				count  = $(target).find('.likes');
+
+			count.text(media_count+1);
+		}
+
+		var likeMediaFail = function(target) {
+			var target = target,
+				hover  = $(target).parent('.instagram-hover'),
+			    notice = hover.siblings('.instagram-fail');
+
+			notice.show().delay(700).fadeOut();
+		}
+
+		$.ajax({
+			type: 'GET',
+			dataType: 'json',
+			url: media_like+media_id,
+			success: function(data) {
+				if(data.result == 'success') {
+					likeMediaSuccess(target);
+				} else if (data.result == 'fail') {
+					console.log(data);
+					likeMediaFail(target);
+				}
+			},
+			error: function() {
+				console.log("Failure in BB object social.InstagramModel");
+			}
+		});
+
+	}
+});
 
 /*
  *
@@ -16,7 +56,8 @@ social.InstagramCollection = Backbone.Collection.extend({
 	model: social.InstagramModel,
 	url: '/social/instagram',
 	parse: function(response) {
-		return response;
+		// Instagram model collection is returned under data
+		return response.content.data;
 	},
 	sync: function(method, model, options) {
 		var params = _.extend({
@@ -38,7 +79,7 @@ social.InstagramView = Backbone.View.extend({
 	el: '#social',
 	feed: {},
 	events: {
-		'click .details': 'likeMedia'
+		'click .details': 'clickMedia'
 	},
 	initialize: function() {
 		this.collection = new social.InstagramCollection();
@@ -77,38 +118,15 @@ social.InstagramView = Backbone.View.extend({
 			}
 		});
 	},
-	likeMedia: function(e) {
-		var media 	    = e.currentTarget,
-			media_id    = media.getAttribute('data-id'),
-			media_count = Number(media.getAttribute('data-likes')),
-			media_like  = '/social/media_like?id=';
+	clickMedia: function(e) {
+		var target	    = e.currentTarget,
+			media_id    = media.getAttribute('data-id');
 
-		var likeMediaSuccess = function() {
-			var count = $(media).find('.likes');
-			count.text(media_count+1);
-		}
-
-		var likeMediaFail = function() {
-			var hover  = $(media).parent('.instagram-hover'),
-			    notice = hover.siblings('.instagram-fail');
-
-			notice.show().delay(700).fadeOut();
-		}
-
-		$.ajax({
-			type: 'GET',
-			dataType: 'json',
-			url: media_like+media_id,
-			success: function(data) {
-				if(data.result == 'success') {
-					likeMediaSuccess();
-				} else if (data.result == 'fail') {
-					console.log(data);
-					likeMediaFail();
-				}
-			}
+		var model = this.collection.findWhere({
+			id: media_id
 		});
 
+		model.likeMedia(target);
 	}
 });
 

f78067dd1e81463c2a8f0331da9fae7d55df4abd
Francis Mitra <francismitra@gmail.com>
2014-11-05 14:08:42 -0500

    Fix bug causing reference to previous models being overwritten

--- static/scripts/social.js
+++ static/scripts/social.js
@@ -74,6 +74,7 @@ VIEWS
 social.InstagramView = Backbone.View.extend({
 	el: '#social',
 	query: {},
+	cache: {},
 	events: {
 		'click .details': 'fireModel',
 		'click #load-more': 'fetchData'
@@ -96,14 +97,14 @@ social.InstagramView = Backbone.View.extend({
 	render: function() {
 		var images = {};
 
-		var models = this.collection.models;
+		// var models = this.collection.models;
 
 		// Parse Instagram collection
-		for(var i = 0; i < models.length; i++) {
-			var photo   = models[i].attributes.images.standard_resolution.url,
-				caption = models[i].attributes.caption == null ? '' : models[i].attributes.caption.text,
-				likes   = models[i].attributes.likes.count,
-				id      = models[i].attributes.id;
+		for(var i = 0; i < this.cache.length; i++) {
+			var photo   = this.cache[i].images.standard_resolution.url,
+				caption = this.cache[i].caption == null ? '' : this.cache[i].caption.text,
+				likes   = this.cache[i].likes.count,
+				id      = this.cache[i].id;
 
 			// Prep images object to dump on template
 			images[i]   = {'photo': photo, 'caption': caption, 'likes': likes, 'id': id};
@@ -113,19 +114,23 @@ social.InstagramView = Backbone.View.extend({
 		    template = _.template($('#instagram-template').html());
 
 		holder.append(template({ collection: images }));
-
+		
 	},
 	fetchData: function() {
 		var self = this;
 		this.collection.fetch({
+			remove: false,
 			data: self.query,
 			success: function(collection, response) {
 				if(response.result == 'success') {
 
+					// Store an reference of the most recently retrieved models
+					self.cache = response.content.data;
+
 					var max_id = response.content.pagination.next_max_id;
 					self.query = {'max_id':max_id};
 
-					// console.log(collection);
+					// console.log(response.content.data);
 
 				} else {
 					console.log(response)

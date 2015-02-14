bdb11482a7ff9d81f2dc2f48d041f3f8d0990cac
Francis Mitra <francismitra@gmail.com>
2015-02-15 02:25:03 +0800

    Fix formating for styles and scripts

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,66 +1,66 @@
 var portfolio = {}
 
 portfolio.DefaultView = Backbone.View.extend({
-	el: 'body',
-	events: { 
-		'click .mobile-shade': 'closeMenu'
-	},
-	initialize: function() {
-		var self = this;
+    el: 'body',
+    events: { 
+        'click .mobile-shade': 'closeMenu'
+    },
+    initialize: function() {
+        var self = this;
 
-		// Enable horizontal scrolling on portfolio
-		$('html, body').mousewheel(function(event, delta) {
-			this.scrollLeft -= (delta * 10); 
-		});
+        // Enable horizontal scrolling on portfolio
+        $('html, body').mousewheel(function(event, delta) {
+            this.scrollLeft -= (delta * 10); 
+        });
 
-		// Lazy load images
-		$('img.unveil').unveil();
+        // Lazy load images
+        $('img.unveil').unveil();
 
-		// Check to close mobile menu if open on desktop view
-		window.addEventListener('resize', function() {
-			if(document.documentElement.clientWidth > 768) {
-				self.removeMobileStyles();
-			}
-		});
+        // Check to close mobile menu if open on desktop view
+        window.addEventListener('resize', function() {
+            if(document.documentElement.clientWidth > 768) {
+                self.removeMobileStyles();
+            }
+        });
 
-		// Initialize animated menu icon
-		new svgIcon( document.querySelector( '.menu-toggle' ), 
-			svgIconConfig, { 
-				easing		: mina.elastic, 
-				speed		: 600,
-				size		: { w : 35, h : 35 },
-				onToggle	: function() {
+        // Initialize animated menu icon
+        new svgIcon( document.querySelector( '.menu-toggle' ), 
+            svgIconConfig, { 
+                easing      : mina.elastic, 
+                speed       : 600,
+                size        : { w : 35, h : 35 },
+                onToggle    : function() {
 
-					$('nav').toggleClass('animate-nav');
-					$('body').toggleClass('animate-body');
-					$('.mobile-menu').toggleClass('animate-body turn-white');
-					$('.mobile-shade').toggle();
+                    $('nav').toggleClass('animate-nav');
+                    $('body').toggleClass('animate-body');
+                    $('.mobile-menu').toggleClass('animate-body turn-white');
+                    $('.mobile-shade').toggle();
 
-				}
-			} );
-	},
-	closeMenu: function() {
-		$('.menu-toggle').trigger('click');
-	},
-	removeMobileStyles: function() {
-		$('.mobile-shade').hide();
+                }
+            } );
+    },
+    closeMenu: function() {
+        $('.menu-toggle').trigger('click');
+    },
+    removeMobileStyles: function() {
+        $('.mobile-shade').hide();
 
-		var remove_classes  = ['animate-nav', 'animate-body', 'turn-white'],
-			mobile_classes  = ['nav', 'body', '.mobile-menu'];
+        var remove_classes  = ['animate-nav', 'animate-body', 'turn-white'],
+            mobile_classes  = ['nav', 'body', '.mobile-menu'];
 
-		_.each(mobile_classes, function(mClass) {
+        _.each(mobile_classes, function(mClass) {
 
-			// convert to jQuery object
-			var thisClass = $(mClass);
+            // convert to jQuery object
+            var thisClass = $(mClass);
 
-			_.each(remove_classes, function(rClass) {
-				if(thisClass.hasClass(rClass)) {
-					thisClass.removeClass(rClass);
-				}
-			});
+            _.each(remove_classes, function(rClass) {
+                if(thisClass.hasClass(rClass)) {
+                    thisClass.removeClass(rClass);
+                }
+            });
 
-		});
-	}
+        });
+    }
 });
 
 portfolio.defaultview = new portfolio.DefaultView;

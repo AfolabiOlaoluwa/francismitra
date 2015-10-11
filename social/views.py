980b1b1b832ab91e97ef3ec8684c12a922d03fc8
Francis Mitra <francismitra@gmail.com>
2015-10-11 21:48:42 +0800

    Update requirements file and replace the JSONResponseMixin class with the default Django option

--- social/views.py
+++ social/views.py
@@ -5,6 +5,7 @@ from django import http
 from social.models import PersonalInstagram, AuthenticateInstagram
 from social.config import INSTAGRAM_CONFIG
 from portfolio.views import LayoutView
+from django.http import JsonResponse
 
 
 """
@@ -16,27 +17,12 @@ instagram_user = AuthenticateInstagram(INSTAGRAM_CONFIG['client_id'],
                                        INSTAGRAM_CONFIG['client_secret'], 
                                        INSTAGRAM_CONFIG['redirect_uri'])
 
-class JSONResponseMixin(object):
-    # Returns a JSON response containing 'context' as payload
-    def render_to_response(self, context):
-        return self.get_json_response(self.convert_context_to_json(context))
-
-    # Construct an `HttpResponse` object.
-    def get_json_response(self, content, **httpresponse_kwargs):
-        return http.HttpResponse(content,
-                                 content_type='application/json',
-                                 **httpresponse_kwargs)
-
-    # Convert the context dictionary into a JSON object
-    def convert_context_to_json(self, context):
-        return json.dumps(context)
-
 """
 Personal Instagram feed rendered as JSON response.
 Does not require authentication and is an instance of
 a separate model
 """ 
-class InstagramFeed(JSONResponseMixin, View):
+class InstagramFeed(View):
     def get(self, request, *args, **kwargs):
         my_instagram = PersonalInstagram(INSTAGRAM_CONFIG['user_id'], 
                                          INSTAGRAM_CONFIG['client_id'])
@@ -50,7 +36,7 @@ class InstagramFeed(JSONResponseMixin, View):
             json_response = {'result':'fail', 'content':'Failure in Django class InstagramFeed'}
 
         context = json_response
-        return self.render_to_response(context)
+        return JsonResponse(context)
 
 
 """
@@ -84,7 +70,7 @@ class SocialView(LayoutView, TemplateView):
 """
 Allow authenticated users to 'like' personal Instagram feed
 """
-class MediaLike(JSONResponseMixin, View):
+class MediaLike(View):
     def get(self, request, *args, **kwargs):
         if self.request.session.get('access_token'):
             token = self.request.session.get('access_token');
@@ -104,7 +90,7 @@ class MediaLike(JSONResponseMixin, View):
 
         context = json_response
 
-        return self.render_to_response(context)
+        return JsonResponse(context)
 
 
 
